0f8e52bceefa1d8e3028a58a9c7a8e2dd737b893
Priyanka Thakan <priyankawu@yahoo.com>
2021-02-17 12:30:03 -0800

    Form to add new trail shows up!

--- AppBuilder9000/HikingFunApp/models.py
+++ AppBuilder9000/HikingFunApp/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+DIFFICULTY_CHOICES = {
+    ('easy', 'easy'),
+    ('medium', 'medium'),
+    ('hard', 'hard'),
+}
+STATE_CHOICES = {
+    ('Washington', 'WA'),
+    ('Missouri', 'MO'),
+    ('Michigan', 'MI'),
+    ('California', 'CA'),
+    ('New York', 'NY'),
+    ('Pennsylvania', 'PA'),
+}
+
+
+# making blueprint of a type called Trails
+class Trails(models.Model):
+    name = models.CharField(max_length=60)
+    state = models.CharField(max_length=10, choices=STATE_CHOICES)
+    miles = models.DecimalField(default=0.0, max_digits=3, decimal_places = 2)
+    difficulty = models.CharField(max_length=10, choices=DIFFICULTY_CHOICES)
+    camping = models.BooleanField()
+
+    objects = models.Manager()
+    def __str__(self):
+        return self.name
--- AppBuilder9000/HikingFunApp/views.py
+++ AppBuilder9000/HikingFunApp/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
-
+from .forms import NewTrailForm
 
 # Create your views here.
 def home(request):
     return render(request, "HikingFunApp/HikingFunApp_home.html")
 
 
+def new_trail(request):
+    # creaste object of form
+    form = NewTrailForm(request.POST or None)
+    # check if form data is valid
+    if form.is_valid():
+        #save the form data to model
+        form.save()
 
+    context = {'form': form}
+    return render(request, "HikingFunApp/new_trail.html", context)
 
 
