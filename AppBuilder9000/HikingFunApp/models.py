b466e8a62dd344db7c68f2538337895fbdd766eb
Priyanka Thakan <priyankawu@yahoo.com>
2021-02-18 16:08:40 -0800

    There was duplicate code in models.py. I deleted that.

--- AppBuilder9000/HikingFunApp/models.py
+++ AppBuilder9000/HikingFunApp/models.py
@@ -25,25 +25,5 @@ class Trails(models.Model):
     def __str__(self):
         return self.name
 
-DIFFICULTY_CHOICES = [
-    ('easy', 'easy'),
-    ('medium', 'medium'),
-    ('hard', 'hard'),
-]
 
-CAMPING_CHOICES = [
-    ('Yes','Yes'),
-    ('No','No'),
-]
 
-# making blueprint of a type called Trails
-class Trails(models.Model):
-    name = models.CharField(max_length=40, default="")
-    location = models.CharField(max_length=80, default="")
-    roundtripMiles = models.DecimalField(default=0.0, max_digits=3, decimal_places=1)
-    difficulty = models.CharField(max_length=10, choices=DIFFICULTY_CHOICES)
-    camping = models.CharField(max_length=10, choices=CAMPING_CHOICES)
-
-    objects = models.Manager()
-    def __str__(self):
-        return self.name
