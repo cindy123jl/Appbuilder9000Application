28938f69b448a217e4c8131f1cf2d708b3f6e234
Libbybacon <libbybacon@github.com>
2021-02-23 11:02:14 -0500

    Added image link to Appbuilder9000 home page

--- AppBuilder9000/AppBuilder9000/urls.py
+++ AppBuilder9000/AppBuilder9000/urls.py
@@ -50,7 +50,7 @@ urlpatterns = [
     path('UpcomingConcertsApp/', include('UpcomingConcertsApp.urls')),
     path('HikingFunApp/', include('HikingFunApp.urls')),
     path('BookClubApp/', include('BookClubApp.urls')),
-    path('CampSite/', includ('CampSite.urls')),
+    path('CampSite/', include('CampSite.urls')),
 ]
 
 #urlpatterns +=staticfiles_urlpatterns()
