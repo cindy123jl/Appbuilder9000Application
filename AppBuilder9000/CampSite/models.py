8af4d02cb4cdf2403240319e0cb240d871f3e82e
Libbybacon <libbybacon@github.com>
2021-02-24 14:48:52 -0500

    Created models for Campsite and MySite objects

--- AppBuilder9000/CampSite/models.py
+++ AppBuilder9000/CampSite/models.py
@@ -1,3 +1,57 @@
 from django.db import models
 
-# Create your models here.
+STATE_CHOICES = [
+    ('CA', 'California'),
+    ('OR', 'Oregon'),
+    ('WA', 'Washington'),
+]
+
+TYPE_CHOICES = [
+    ('Dispersed', 'Dispersed'),
+    ('Developed', 'Developed'),
+]
+
+ACCESS_CHOICES = [
+    ('Drive-up', 'Drive-up'),
+    ('Hike-in', 'Hike-in'),
+]
+
+RATING_CHOICES = [
+    ('1', '1'),
+    ('2', '2'),
+    ('3', '3'),
+    ('4', '4'),
+    ('5', '5'),
+    ('6', '6'),
+    ('7', '7'),
+    ('8', '8'),
+    ('9', '9'),
+    ('10', '10'),
+]
+
+
+class Campsite(models.Model):
+
+    name = models.CharField(max_length=50)
+    state = models.CharField(max_length=2, choices=STATE_CHOICES)
+    type = models.CharField(max_length=9, choices=TYPE_CHOICES)
+    access = models.CharField(max_length=8, choices=ACCESS_CHOICES)
+    description = models.CharField(max_length=500)
+    fee = models.DecimalField(max_digits=5, decimal_places=2)
+    directions = models.CharField(max_length=500, null=True, blank=True)
+
+    Campsites = models.Model()
+
+    def __str__(self):
+        return self.name + "," + self.state
+
+
+class MySite(models.Model):
+    name = models.ForeignKey(Campsite, on_delete=models.CASCADE)
+    state = models.CharField(max_length=2, choices=STATE_CHOICES)
+    type = models.CharField(max_length=9, choices=TYPE_CHOICES)
+    access = models.CharField(max_length=8, choices=ACCESS_CHOICES)
+    rating = models.IntegerField(max_length=2, choices=RATING_CHOICES)
+    notes = models.CharField(max_length=250, null=True, blank=True)
+
+    MySites = models.Model()
