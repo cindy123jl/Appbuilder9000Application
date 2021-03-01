2f8ce659f898a8bb01afeff2790ff22c70f4bdae
Libbybacon <libbybacon@github.com>
2021-03-01 00:26:19 -0500

    Added a delete function to the campsite details page, with a modal that confirms the delete.
    Changed a lot of styling.

--- AppBuilder9000/CampSite/urls.py
+++ AppBuilder9000/CampSite/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('browse_campsites/', views.browse, name='browse'),
     path('campsite_details/<int:campsite_id>/', views.campsite_details, name='campsite_details'),
     path('campsite_details/<int:campsite_id>/edit', views.edit_campsite, name='edit_campsite'),
-
+    path('campsite_details/<int:campsite_id>/delete', views.delete_campsite, name='delete_campsite'),
 ]
