6c618559f88a32b1df3763c55977c29e2c388ab7
DJMarcos <62577982+DJMarcos@users.noreply.github.com>
2021-03-02 11:13:18 -0800

    so far worked on the home index and added an image for my topic, worked on little on models

--- AppBuilder9000/quantumapp/models.py
+++ AppBuilder9000/quantumapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+
+class quantuminfo(models.Model):
+    quantum_computers = models.CharField(max_length=200)
+    quantumtheorie = models.CharField(max_length=200)
+    superposition = models.CharField(max_length=200)
+
+
---
+++ AppBuilder9000/quantumapp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
+
+urlpatterns = [
+    path('quantumapp/', include('quantumapp.urls')),
+]
