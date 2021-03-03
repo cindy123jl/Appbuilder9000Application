42055b9c9c97193af42ed692ab8afecd224c00ad
Chris Robinson <oops.ninja@outlook.com>
2021-03-03 07:56:08 -0800

    Worked on moving some HTML into 2 seperate templates, also debugging some template errors. Things seem to be working as needed for story 1

--- AppBuilder9000/AwesomeWeather/views.py
+++ AppBuilder9000/AwesomeWeather/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
 import requests
+from django.shortcuts import render
 from .models import City
 from .forms import CityForm
 
@@ -8,31 +8,32 @@ from .forms import CityForm
 
 
 def home(request):
-    cities = City.objects.all()
 
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=84ed84d576a60dbcbf4149fa354c5cfe'
 
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=271d1234d3f497eed5b1d80a07b3fcd1'
-    form = CityForm()
     if request.method == 'POST':  # only true if form is submitted
         form = CityForm(request.POST)  # add actual request data to form for processing
         form.save()  # will validate and save if validate
 
+    form = CityForm()
 
+
+    cities = City.objects.all()
 
     weather_data = []
 
     for city in cities:
-        city_weather = requests.get(
-            url.format(city)).json()  # request the API data and convert the JSON to Python data types
 
-        weather = {
-            'city': city,
-            'temperature': city_weather['main']['temp'],
-            'description': city_weather['weather'][0]['description'],
-            'icon': city_weather['weather'][0]['icon']
+        r = requests.get(url.format(city)).json()
+
+        city_weather = {
+            'city': city.name,
+            'temperature': r['main']['temp'],
+            'description': r['weather'][0]['description'],
+            'icon': r['weather'][0]['icon']
         }
 
-        weather_data.append(weather)  # add the data for the current city into our list
+        weather_data.append(city_weather)  # add the data for the current city into our list
 
     context = {'weather_data': weather_data, 'form': form}
 
