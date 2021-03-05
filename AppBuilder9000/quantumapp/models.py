d2223ce7a3c976bd27167508e2fca04e3bb1f5b5
DJMarcos <62577982+DJMarcos@users.noreply.github.com>
2021-03-04 16:48:05 -0800

    worked on the basic layout of my website

--- AppBuilder9000/quantumapp/models.py
+++ AppBuilder9000/quantumapp/models.py
@@ -1,11 +1,2 @@
-from django.db import models
-import datetime
-
-# Create your models here.
-
-class quantuminfo(models.Model):
-    quantum_computers = models.CharField(max_length=200)
-    quantumtheorie = models.CharField(max_length=200)
-    superposition = models.CharField(max_length=200)
 
 
