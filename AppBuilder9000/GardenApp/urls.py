c3481d7c9c3db7d14b6f64987bdf61029c0a6b3d
esievaughn <esievaughn@gmail.com>
2021-03-16 07:47:22 -0700

    linking background color

--- AppBuilder9000/GardenApp/urls.py
+++ AppBuilder9000/GardenApp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name='gardenhome'),
+    path('gardenhome/', views.home, name='garden_home'),
     path('gardenplanner/', views.gardenplanner, name='gardenplanner'),
     path('contact/', views.contact, name='contact'),
 ]
