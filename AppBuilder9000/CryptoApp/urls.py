f754663797c8e3fc2914012c5a13af05e990b860
Jason Fors <jasonfors@hotmail.com>
2021-03-26 11:00:45 -0400

    Add details views for each currency linking from display page.

--- AppBuilder9000/CryptoApp/urls.py
+++ AppBuilder9000/CryptoApp/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.home, name='CryptoApp_home'),
     path('create/', views.add_currency, name='CryptoApp_AddCurrency'),
     path('update/', views.add_status, name='CryptoApp_AddStatus'),
-    path('display/', views.display, name='CryptoApp_display')
+    path('display/', views.display, name='CryptoApp_display'),
+    path('details/<int:record_id>/', views.details, name='CryptoApp_details')
 ]
