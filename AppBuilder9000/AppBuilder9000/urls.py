fe609aab60a7819a730a17145d00df9182622fe6
srnovak13 <srnovak@gmail.com>
2021-03-25 17:52:33 -0700

    Add NovakaneWeather app

--- AppBuilder9000/AppBuilder9000/urls.py
+++ AppBuilder9000/AppBuilder9000/urls.py
@@ -25,7 +25,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('AwesomeWeather/', include('AwesomeWeather.urls')),
     path('BudgetingApp/', include('BudgetingApp.urls')),
-    path('GardenApp/', include('GardenApp.urls'))
+    path('GardenApp/', include('GardenApp.urls')),
+    path('NovakaneWeather/', include('NovakaneWeather.urls')),
 ]
 
 #urlpatterns +=staticfiles_urlpatterns()
