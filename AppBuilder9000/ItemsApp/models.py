5bf4e6f669b29a38ec0fbb93a2aadf80a80876c4
Alex Sanderson <asanderson1994s@gmail.com>
2021-03-31 19:32:11 -0700

    Commit for end of day

--- AppBuilder9000/ItemsApp/models.py
+++ AppBuilder9000/ItemsApp/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    # General Information
+    type = models.CharField(max_length=20, default="", blank=True)
+    name = models.CharField(max_length=20, default="", blank=True)
+    # Attack Bonuses
+    atk_stab = models.IntegerField()
+    atk_slash = models.IntegerField()
+    atk_crush = models.IntegerField()
+    atk_magic = models.IntegerField()
+    atk_range = models.IntegerField()
+    # Defence Bonuses
+    def_stab = models.IntegerField()
+    def_slash = models.IntegerField()
+    def_crush = models.IntegerField()
+    def_magic = models.IntegerField()
+    def_range = models.IntegerField()
+    # Other Bonuses
+    bns_strength = models.IntegerField()
+    bns_range = models.IntegerField()
+    bns_magic = models.IntegerField()
+    bns_prayer = models.IntegerField()
+    # Weapon Slot
+    slot = models.CharField(max_length=60, default="", blank=True)
+    # Weapon Speed
+    speed = models.IntegerField()
+    # Range
+    range = models.IntegerField()
+    # Images
+    image = models.CharField(max_length=255, default="", blank=True)
+
+    # Renaming the models manager to a new variable
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.name
--- AppBuilder9000/ItemsApp/views.py
+++ AppBuilder9000/ItemsApp/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 # Create your views here.
 def home(request):
-    return render(request, 'ItemsApp/ItemsApp_home.html')
+    return render(request, 'ItemsApp/home.html')
+
+
+def new_item(request):
+    return render(request, 'ItemsApp/new_item.html')
