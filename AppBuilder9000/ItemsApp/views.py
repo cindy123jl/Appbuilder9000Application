35dde582187d805187055fa93c2cbb9e55091d0e
Alex Sanderson <asanderson1994s@gmail.com>
2021-04-02 11:20:08 -0700

    Change urls.py home name

--- AppBuilder9000/ItemsApp/views.py
+++ AppBuilder9000/ItemsApp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Item
 from .forms import ItemForm
+from .models import Item
 
 
 # Create your views here.
@@ -30,3 +30,22 @@ def createItem(request):
         'form': form,
     }
     return render(request, 'ItemsApp/createItem.html', context)
+
+
+def viewItem(request):
+    return render(request, 'ItemsApp/displayItems.html')
+
+
+def details(request, pk):
+    pk = int(pk)
+    item = get_object_or_404(Item, pk=pk)
+    form = ItemForm(data=request.POST or None, instance=item)
+    if request.method == 'POST':
+        if form.is_valid():
+            form2 = form.save(commit=False)
+            form2.save()
+            return redirect('home')
+        else:
+            print(form.errors)
+    else:
+        return render(request, 'ItemsApp/new_item.html', {'form': form})
