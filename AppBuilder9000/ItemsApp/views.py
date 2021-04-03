77033b312fef59f3fac182d228b15272f7e065e4
Alex Sanderson <asanderson1994s@gmail.com>
2021-04-03 16:17:48 -0700

    Commit

--- AppBuilder9000/ItemsApp/views.py
+++ AppBuilder9000/ItemsApp/views.py
@@ -22,6 +22,6 @@ def new_item(request):
 
 def display_items(request):
     """Creates a form displaying the current items in the database."""
-    item = Item.objects.all()
+    item = Item.objects.all().values()
     content = {'item': item}
     return render(request, 'ItemsApp/display_items.html', content)
