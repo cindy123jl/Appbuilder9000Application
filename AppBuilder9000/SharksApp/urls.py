e27656f1b22dbe8a6c534231212504cbfa4da2c9
MurrinB <britneemurrin@yahoo.com>
2021-04-08 18:34:13 -0600

    Add user friendly form page.

--- AppBuilder9000/SharksApp/urls.py
+++ AppBuilder9000/SharksApp/urls.py
@@ -6,5 +6,6 @@ from . import views
 
 
 urlpatterns = [
-    path('SharksApp_home', views.SharksApp_home, name="SharksApp_home")
+    path('SharksApp_home', views.SharksApp_home, name="SharksApp_home"),
+    path('SharksApp_newitem/', views.Create_Shark, name="SharksApp_newitem"),
 ]
--- AppBuilder9000/SharksApp/views.py
+++ AppBuilder9000/SharksApp/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .forms import SharksForm
+
 
 def SharksApp_home(request):
     return render(request, "SharksApp_home.html")
+
+
+def Create_Shark(request):
+    form = SharksForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('SharksApp_newitem')
+    else:
+        print(form.errors)
+        form = SharksForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'SharksApp/templates/SharksApp_newitem.html', context)
+
 
