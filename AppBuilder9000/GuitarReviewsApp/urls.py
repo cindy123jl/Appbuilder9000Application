7d0d2c207da8f8650fa5029003c7674a24ad2421
jerry-nol <newyork_14@live.com>
2021-04-08 18:55:34 -0600

    Story1

--- AppBuilder9000/GuitarReviewsApp/urls.py
+++ AppBuilder9000/GuitarReviewsApp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 
-urlspatterns = [
-    path('', views.home, name='GuitarReviews_home.html'),
+urlpatterns = [
+    path('', views.home, name='GuitarReviews_home'),
 ]
