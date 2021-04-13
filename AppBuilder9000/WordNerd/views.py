e96abf9815ab9e076107c329869f06802d514c61
Joe <josephomalley135@gmail.com>
2021-04-13 01:53:05 -0500

    -remove etymology table -add word origin -add viewwords HTML template

--- AppBuilder9000/WordNerd/views.py
+++ AppBuilder9000/WordNerd/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .forms import wordform, etymologyform
+from .forms import wordform
+from .models import word
 
 def WordNerd_home(request):
     return render(request, 'wordnerd/WordNerd_home.html')
@@ -11,17 +12,13 @@ def WordNerd_createword(request):
         wform = wordform(request.POST)
         if wform.is_valid():
             wform.save()
-
-    eform = etymologyform()
-    if request.method == 'POST':
-        print(request.POST)
-        eform = etymologyform(request.POST)
-        if eform.is_valid():
-            eform.save()
-
-    context = {
-        'wform': wform,
-        'eform': eform,
-    }
+    context = {'wform': wform}
     return render(request, 'wordnerd/WordNerd_createword.html', context)
+
+def WordNerd_viewdata(request):
+    wAll = word.objects.all
+    context = {
+        'wAll': wAll
+    }
+    return render(request, "wordnerd/WordNerd_viewdata.html", context)
 
