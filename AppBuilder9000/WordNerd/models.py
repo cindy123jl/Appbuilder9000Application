e0a5ea010689fd8413fe61539be2097f6bc8da4d
Joe <josephomalley135@gmail.com>
2021-04-13 17:33:01 -0500

    -add Unversioned files -update var names to be more specific.

--- AppBuilder9000/WordNerd/models.py
+++ AppBuilder9000/WordNerd/models.py
@@ -5,7 +5,7 @@ class word(models.Model):
     word_definition = models.CharField(max_length=255)
     word_in_a_sentence = models.CharField(max_length=255)
     where_did_you_find_the_word = models.CharField(max_length=255, null=True)
-    origin = models.CharField(max_length=255, null=True)
+    parent_languages = models.CharField(max_length=255, null=True)
 
     objects = models.Manager()
 
--- AppBuilder9000/WordNerd/views.py
+++ AppBuilder9000/WordNerd/views.py
@@ -6,19 +6,19 @@ def WordNerd_home(request):
     return render(request, 'wordnerd/WordNerd_home.html')
 
 def WordNerd_createword(request):
-    wform = wordform()
+    wordForm = wordform()
     if request.method == 'POST':
         print(request.POST)
-        wform = wordform(request.POST)
-        if wform.is_valid():
-            wform.save()
-    context = {'wform': wform}
+        wordForm = wordform(request.POST)
+        if wordForm.is_valid():
+            wordForm.save()
+    context = {'wordForm': wordForm}
     return render(request, 'wordnerd/WordNerd_createword.html', context)
 
 def WordNerd_viewdata(request):
-    wAll = word.objects.all
+    allWords = word.objects.all
     context = {
-        'wAll': wAll
+        'allWords': allWords
     }
     return render(request, "wordnerd/WordNerd_viewdata.html", context)
 
