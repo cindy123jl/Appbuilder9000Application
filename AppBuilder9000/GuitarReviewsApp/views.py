1252467862bdf023b65cc4ecb401cf16771063df
jerry-nol <newyork_14@live.com>
2021-04-13 18:53:34 -0600

    Story #2

--- AppBuilder9000/GuitarReviewsApp/views.py
+++ AppBuilder9000/GuitarReviewsApp/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import GuitarForm
+
 
 # Create your views here.
 def home(request):
-    print("Home method")
     return render(request, 'GuitarReviewsApp/GuitarReviewsApp_home.html')
 
+
+def create(request):
+    form = GuitarForm(request.POST or None)
+    if request.method == "POST":
+        # check if form is valid
+        # if so, save
+        if form.is_valid():
+            form.save()
+            return redirect("GuitarReviews_create")
+
+    context = {'form': form}
+    return render(request, "GuitarReviewsApp/GuitarReviewsApp_create.html", context)
