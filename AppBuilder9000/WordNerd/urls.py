f8adc3d64e34f8adf6d533ea29929c469e11b49e
Joe <josephomalley135@gmail.com>
2021-04-15 18:20:04 -0500

    - add url path the displays specific word - add a view function that displays specific word via the word id - modifiy css to change anchor tag styling precedence - add word details page

--- AppBuilder9000/WordNerd/urls.py
+++ AppBuilder9000/WordNerd/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from .import views
-
+# The url name is used to redirected the user via views.py to the url path na
 urlpatterns = [
     path('', views.WordNerd_home, name='WordNerd_home'),
     path('WordNerd_createword/', views.WordNerd_createword, name='WordNerd_createword'),
-    path('WordNerd_viewdata/', views.WordNerd_viewdata, name='WordNerd_viewdata')
+    path('WordNerd_viewdata/', views.WordNerd_viewdata, name='WordNerd_viewdata'),
+    path('<int:pk>/WordNerd_worddetails/', views.WordNerd_worddetails, name="WordNerd_worddetails"),
 ]
--- AppBuilder9000/WordNerd/views.py
+++ AppBuilder9000/WordNerd/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .forms import wordform
 from .models import word
+from django.http import HttpResponse
 
 def WordNerd_home(request):
     return render(request, 'wordnerd/WordNerd_home.html')
@@ -21,4 +22,12 @@ def WordNerd_viewdata(request):
         'allWords': allWords
     }
     return render(request, "wordnerd/WordNerd_viewdata.html", context)
+#uses primary key as a filter tag for selecting the chosen word
+def WordNerd_worddetails(request, pk):
+    pk = int(pk)
+    word_get = word.objects.filter(pk=pk)
+    context = {'word_get': word_get}
+    return render(request, "wordnerd/WordNerd_worddetails.html", context)
+
+
 
