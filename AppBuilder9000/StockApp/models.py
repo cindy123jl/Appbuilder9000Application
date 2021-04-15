caf250f630b24ed8cd6bbad643ff413974f9b456
Reece <ReeceHarris423@gmail.com>
2021-04-15 16:41:23 -0600

    Story 2 Created a Form and Model Made 3 objects Test1 Test2 and Test3 Styling is still minimal.

--- AppBuilder9000/StockApp/models.py
+++ AppBuilder9000/StockApp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Stock(models.Model):
+    name = models.CharField(max_length=5)
+    market_cap = models.CharField(max_length=30)
+    time_stamp = models.TimeField(max_length=40)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    Stock = models.Manager()
+
+
+
