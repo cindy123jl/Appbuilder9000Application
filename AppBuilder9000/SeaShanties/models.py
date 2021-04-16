eb9f06799a63aabb8082e45a84f61a0f8149bd17
Keen L <meisterkeen@gmail.com>
2021-04-16 10:02:16 -0700

    Add SeaShanty home page and styles

--- AppBuilder9000/SeaShanties/models.py
+++ AppBuilder9000/SeaShanties/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.db.models import Manager
 
 # Create your models here.
+
+
+class SeaShanties(models.Model):
+    title = models.CharField(max_length=60)
+    videoURL = models.URLField()
+    lyrics = models.TextField()
+    artist = models.CharField(max_length=60)
+    year = models.CharField(max_length=60)
+    history = models.TextField()
+
+    def __str__(self):
+        return self.title
