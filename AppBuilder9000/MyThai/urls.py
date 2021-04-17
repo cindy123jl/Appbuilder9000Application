b08a919c8848836feffab440f0852613e72d5f4d
Nathan Skiles <nateskiles94@gmail.com>
2021-04-16 17:22:55 -0700

    Add details page for each item. -NateS

--- AppBuilder9000/MyThai/urls.py
+++ AppBuilder9000/MyThai/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('my_restaurants/', views.my_restaurants_view, name="MyThai_my_restaurants"),
     path('add_restaurant/', views.new_restaurant, name="MyThai_add_restaurant"),
     path('add_dish/', views.new_dish, name="MyThai_add_dish"),
+    path('<int:pk>/details/', views.details, name="MyThai_details"),
 ]
--- AppBuilder9000/MyThai/views.py
+++ AppBuilder9000/MyThai/views.py
@@ -1,6 +1,6 @@
 from django.db.models import Q
 from django.core.paginator import Paginator
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import RestaurantForm, DishForm
 from .models import Restaurant, Dish
 from operator import attrgetter
@@ -43,17 +43,20 @@ def my_restaurants_view(request):
     get_dish_query = request.GET.get('get_dish')
     my_sort = request.GET.get('dishes')
 
-    if is_valid_query(get_dish_query):     # If is valid = True, filter queryset
+    if is_valid_query(get_dish_query):  # If is valid = True, filter queryset
         dish_list = dish_list.filter(
             Q(dishName__icontains=get_dish_query) |
-            Q(restaurant__name__icontains=get_dish_query)).distinct()   # Only return distinct entries
+            Q(restaurant__name__icontains=get_dish_query)).distinct()  # Only return distinct entries
 
-    dish_list = my_sorted(dish_list, my_sort)                # Sort Dict
+    if my_sort is None:
+        my_sort = 'dishName'
 
-    paginator1 = Paginator(restaurant_list, 10)     # Create paginator object with 10 restaurants per page
+    dish_list = my_sorted(dish_list, my_sort)  # Sort Dict
+
+    paginator1 = Paginator(restaurant_list, 10)  # Create paginator object with 10 restaurants per page
     paginator2 = Paginator(dish_list, 15)
 
-    page = request.GET.get('page')              # Store paginator object with page number
+    page = request.GET.get('page')  # Store paginator object with page number
 
     restaurants = paginator1.get_page(page)
     dishes = paginator2.get_page(page)
@@ -63,13 +66,20 @@ def my_restaurants_view(request):
 
 
 def is_valid_query(param):
-    return param != '' and param is not None    # Makes sure search is valid query, if not return false
+    return param != '' and param is not None  # Makes sure search is valid query, if not return false
 
 
 def my_sorted(dish_list, my_sort):
-    if my_sort == 'rating':             # If sorted by rating, reverse so highest goes at the top.
+    if my_sort == 'rating':  # If sorted by rating, reverse so highest goes at the top.
         asc = True
     else:
         asc = False
-    dish_list = sorted(dish_list, key=attrgetter(my_sort), reverse=asc)     # Sort dict by model attribute = 'my_sort'
+    dish_list = sorted(dish_list, key=attrgetter(my_sort), reverse=asc)  # Sort dict by model attribute = 'my_sort'
     return dish_list
+
+
+def details(request, pk):
+    pk = int(pk)
+    dish = get_object_or_404(Dish, pk=pk)
+    context = {'dish': dish}
+    return render(request, 'MyThai/MyThai_details.html', context)
