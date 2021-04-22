b58a5ae9a4c5d6f6e27bc59ed523aacfdc757ce3
Nathan Skiles <nateskiles94@gmail.com>
2021-04-22 13:55:51 -0700

    Fix issue with edit/delete on edit page.

--- AppBuilder9000/MyThai/urls.py
+++ AppBuilder9000/MyThai/urls.py
@@ -11,8 +11,8 @@ urlpatterns = [
     path('<int:pk>/dish/edit/', views.dish_edit, name="MyThai_dish_edit"),
     path('<int:pk>/dish/delete/', views.dish_delete, name="MyThai_dish_delete"),
     path('<int:pk>/restaurant/', views.restaurant_details, name="MyThai_rest_details"),
-    path('<int:pk>/restaurant/edit', views.restaurant_edit, name="MyThai_rest_edit"),
-    path('<int:pk>/restaurant/delete', views.restaurant_delete, name="MyThai_rest_delete"),
+    path('<int:pk>/restaurant/edit/', views.restaurant_edit, name="MyThai_rest_edit"),
+    path('<int:pk>/restaurant/delete/', views.restaurant_delete, name="MyThai_rest_delete"),
     path('dish_deleted', views.dish_confirmed, name="MyThai_dish_confirmed"),
 
 ]
